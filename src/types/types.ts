051ba0a4ea09c8fe9f4f18c539ff57991f10f141
Emad <eksaber87@gmail.com>
2025-01-07 14:00:38 +0100

    first commit

---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+
+
+export type Char = {
+  isAlive: boolean;
+  message: string;
+  name: string;
+  family: string;
+  age: number;
+  reignYears: number;
+  category: string;
+  weapon?: undefined;
+  skillLevel?: undefined;
+  adviseTo?: undefined;
+  serveLevel?: undefined;
+  servesTo?: undefined;
+};
